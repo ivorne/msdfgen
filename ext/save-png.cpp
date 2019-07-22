98851ff6ff59089fa7a6712f5c53472c4c5949bb
Jan Košař <kosarj@gmail.com>
2019-07-22 15:30:12 +0200

    Reverted change that made msdf fonts store as RGBA, they are now back to RGB.

--- ext/save-png.cpp
+++ ext/save-png.cpp
@@ -18,7 +18,7 @@ bool savePng(const Bitmap<float> &bitmap, const char *filename, bool invertY) {
 }
 
 bool savePng(const Bitmap<FloatRGB> &bitmap, const char *filename, bool invertY) {
-    std::vector<unsigned char> pixels(4*bitmap.width()*bitmap.height());
+    std::vector<unsigned char> pixels(3*bitmap.width()*bitmap.height());
     std::vector<unsigned char>::iterator it = pixels.begin();
     for (int y = 0; y < bitmap.height(); ++y)
         for (int x = 0; x < bitmap.width(); ++x) {
@@ -26,9 +26,8 @@ bool savePng(const Bitmap<FloatRGB> &bitmap, const char *filename, bool invertY)
             *it++ = clamp(int(bitmap(x, yy).r*0x100), 0xff);
             *it++ = clamp(int(bitmap(x, yy).g*0x100), 0xff);
             *it++ = clamp(int(bitmap(x, yy).b*0x100), 0xff);
-            *it++ = 0xfe;
         }
-    return !lodepng::encode(filename, pixels, bitmap.width(), bitmap.height(), LCT_RGBA);
+    return !lodepng::encode(filename, pixels, bitmap.width(), bitmap.height(), LCT_RGB);
 }
 
 }
